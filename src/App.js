d22cd2c2805558ab2edd8fef1214c461d76f0a97
lynqux <57960864+lynqux@users.noreply.github.com>
2020-11-19 13:34:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import {Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NasaImage from './Components/NasaImage';
+import Weather from './Components/Weather';
+import Zomato from './Components/Zomato';
+
 
 function App() {
+  const [lat, setLat] = useState('');
+  const [long, setLong] = useState('');
+
+function componentDidMount(){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  } )
+}
+
+componentDidMount();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Location Details</h1>
+
+      <p>lat: {lat} long: {long}</p>
+      <br/>
+      <NasaImage lat={lat} long={long}/>
+      <br/>
+      <Weather lat={lat} long={long}/>
+      <br/>
+      <Zomato lat={lat} long={long}/>
     </div>
   );
+
 }
 
 export default App;
+
+
---
+++ src/Components/NasaImage.js
@@ -0,0 +1,2 @@
+const { Component } = require("react");
+
