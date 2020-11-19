330b1521b43f92cc1d608ea3fb30365b411e505f
Danilo Uy <danilouy2011@gmail.com>
2020-11-19 18:57:26 -0500

    upload NASA component

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ getLocation();
     <div className="App">
       <h1>Find Info on Your Location!</h1>
       <br/>
-      {/* {lat !=="" && long!=="" ? <NasaImage lat={lat} long={long}/> : <p>Location not Available! Please accept request for location access!</p>} */}
+      {lat !=="" && long!=="" ? <NasaImage lat={lat} long={long}/> : <p>Location not Available! Please accept request for location access!</p>} 
       <br/>
       {/* {lat !=="" && long!=="" ? <Weather lat={lat} long={long}/> : null} */}
       <br/>
---
+++ src/Components/NasaImage.js
@@ -0,0 +1,31 @@
+// import {useEffect, useState} from 'react';
+
+const NasaImage = (props) => {
+
+    console.log(props)
+ 
+    var today = new Date();
+    var inputDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    console.log("inputDate:",inputDate);
+ 
+    const baseURL = "https://api.nasa.gov/planetary/earth/imagery";
+    let apiKey = 'BrjpVmZ1TEcej28c942Hi8qKMEUNe67GU9n34Hui';
+    let finalURL =  baseURL + '?lon=' + props.long + '&lat=' + props.lat + '&' + 'date=' + '2020-10-01' + '&dim=0.15&' + 'api_key=' + apiKey; 
+     
+    // useEffect(() => {
+    //     fetch(finalURL, {
+    //       method: 'GET'                  
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    //   }, [])    
+       
+    return (  
+        <div className="NASAImg">
+         {/* <a href={finalURL} target="_blank">NASA Image</a> */}  
+         <img src={finalURL} width="70%"/>    
+        </div>
+  )  
+}
+
+export default NasaImage;
