dd237059b4969da3fe2ea321bd61cf472f0ae006
Danilo Uy <danilouy2011@gmail.com>
2020-11-20 08:04:11 -0500

    NASA app earliest date accepted

--- src/Components/NasaImage.js
+++ src/Components/NasaImage.js
@@ -1,31 +1,26 @@
-// import {useEffect, useState} from 'react';
-
 const NasaImage = (props) => {
 
     console.log(props)
- 
-    var today = new Date();
-    var inputDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    console.log("inputDate:",inputDate);
- 
+   
+    let numDays = 10; 
+    let date = ( function(){this.setDate(this.getDate()-numDays); return this} )
+               .call(new Date);
+    console.log('date',date);
+
+    var dateStr = date.toISOString().slice(0,10);
+    console.log('dateStr',dateStr);
+
     const baseURL = "https://api.nasa.gov/planetary/earth/imagery";
-    let apiKey = 'BrjpVmZ1TEcej28c942Hi8qKMEUNe67GU9n34Hui';
-    let finalURL =  baseURL + '?lon=' + props.long + '&lat=' + props.lat + '&' + 'date=' + '2020-10-01' + '&dim=0.15&' + 'api_key=' + apiKey; 
+    const apiKey = "BrjpVmZ1TEcej28c942Hi8qKMEUNe67GU9n34Hui";
+    
+    let finalURL =  `${baseURL}?lon=${props.long}&lat=${props.lat}&date=${dateStr}&dim=0.15&api_key=${apiKey}`; 
+    console.log('finalURL',finalURL);
      
-    // useEffect(() => {
-    //     fetch(finalURL, {
-    //       method: 'GET'                  
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
-    //   }, [])    
-       
     return (  
         <div className="NASAImg">
-         {/* <a href={finalURL} target="_blank">NASA Image</a> */}  
-         <img src={finalURL} width="70%"/>    
+          <img src={finalURL} width="70%"/>    
         </div>
-  )  
+    )  
 }
 
 export default NasaImage;
